e37042846dd8ae9b1905d49516a4ac202990ae77
Ram Sauce <ramisouguir@gmail.com>
2024-03-13 16:43:06 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,70 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-user',
+  template: `
+    Username: {{ username }}
+  `,
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+})
+export class UserComponent {
+  username = 'youngTech'
+}
+
+@Component({
+  selector: 'app-os',
+  template: `
+  <ul>
+    @for (os of operatingSystems; track os.id) {
+      <li>
+        {{ os.name }}
+      </li>
+    }
+  </ul>
+  `,
+  standalone: true
+})
+export class OSComponent {
+  operatingSystems = [{id: 'win', name: 'Windows'}, {id: 'osx', name: 'MacOS'}, 
+    {id: 'linux', name: 'Linux'}];
+}
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <section>
+        @if (isLoggedIn){
+          <app-user/>
+        }
+      <br/>
+
+      <p> {{ city }}</p> <br/>
+      
+      <p> 
+        @if(isServerRunning){
+          Server is Running 
+        }
+        @else{
+          Server not running
+        }
+      </p> <br/>
+
+      <app-os/>
+    </section>
+  `,
+  imports: [UserComponent, OSComponent],
+  styles: `
+    :host {
+      color: #a144eb;
+    }
+  `,
+  standalone: true,
+  // imports: [RouterOutlet],
+  // templateUrl: './app.component.html',
+  // styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'tutorial';
+  city='San Fransisco';
+  isLoggedIn = true;
+  isServerRunning = false;
 }
