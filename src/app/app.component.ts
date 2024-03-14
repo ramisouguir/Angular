d7dd81be195f3cad8424790bd45491e085fff37b
Ram Sauce <ramisouguir@gmail.com>
2024-03-14 00:22:38 -0400

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  template: `
+    <button (click)="onClick()">Increment</button>
+  `,
+  standalone: true,
+  styles: `
+    button{
+      margin-right: 10px;
+    }
+      `,
+})
+export class ChildComponent {
+  count = 0
+  @Output() incrementCountEvent = new EventEmitter<number>();
+  onClick() {
+    this.count++;
+    this.incrementCountEvent.emit(this.count);
+    console.log(this.count)
+  }
+}
 
 @Component({
   selector: 'app-user',
   template: `
-    Username: {{ username }}
+    Username: {{ username }}, Occupation: {{occupation}}
   `,
   standalone: true,
 })
 export class UserComponent {
+  @Input() occupation = '';
   username = 'youngTech'
 }
 
@@ -35,28 +59,44 @@ export class OSComponent {
   template: `
     <section>
         @if (isLoggedIn){
-          <app-user/>
+          <app-user occupation="Angular Dev"/>
         }
-      <br/>
+      <hr/>
 
-      <p> {{ city }}</p> <br/>
+      <p> {{ city }}</p> <hr/>
       
       <p> 
         @if(isServerRunning){
           Server is Running 
         }
         @else{
-          Server not running
-        }
-      </p> <br/>
+          Server not Running
+        } 
+      </p> <hr/>
 
-      <app-os/>
+      <app-os/> <hr/>
+
+      <div [contentEditable] = "isEditable">Type Here</div> <hr/>
+      
+      <button (click)="greet()">{{but_text}}</button> <hr/>
+      
+      <p (mouseover)="onMouseOver()">HOVER OVER ME {{ message }}</p> <hr/>
+      
+      <div>
+        <app-child (incrementCountEvent)="setCount($event)"/>
+        <p>{{ app_count }}</p>
+      </div>
     </section>
   `,
-  imports: [UserComponent, OSComponent],
+  imports: [UserComponent, OSComponent, ChildComponent],
   styles: `
     :host {
       color: #a144eb;
+    }
+    div {
+      margin-right: 5px;
+      display: flex;
+      align-items: center;
     }
   `,
   standalone: true,
@@ -68,4 +108,21 @@ export class AppComponent {
   city='San Fransisco';
   isLoggedIn = true;
   isServerRunning = false;
+  isEditable = true;
+  but_text = "CLICK ME";
+  message = "";
+  greet() {
+    console.log('Ello Govna');
+    if(this.but_text.length>1) {
+      this.but_text = this.but_text.substring(0, this.but_text.length-1);
+    }
+    console.log(this.but_text);
+  }
+  onMouseOver() {
+    this.message = "to see this secret message";
+  }
+  app_count = 0;
+  setCount(item: number) {
+    this.app_count = item;
+  }
 }
